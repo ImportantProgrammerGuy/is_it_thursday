40f6c74e91fdd959021eedbb31896d9f0ab6a285
Kobol-Coder <lugalot82@hotmail.com>
2020-11-19 18:59:56 -0800

    Fixed typos

--- is_thursday.py
+++ is_thursday.py
@@ -4,9 +4,9 @@ def is_thursday():
     now = datetime.datetime.now()
     weekday = now.weekday()
     if weekday == 3:   #Monday is 0, Thursday is 3 for weekday
-        message = "It's Tuhrsday!"
+        message = "It's Thursday!"
     else:
-        message = "It's not Tuhrsday."
+        message = "It's not Thursday."
     return message
 
 
